81ba4c0a62a98e1fd910e8f37f04ea353e9b3293
Ahmed Atef Hegazy <ahmedatef10987@gmail.com>
2026-02-14 14:07:40 +0200

    made the FindPerson Form

---
+++ DVDL/Forms/PeopleManagement/Forms/FindPersonForm/FindPersonForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DVDL.Forms.PeopleManagement.Forms.FindPersonForm
+{
+    public partial class FindPersonForm : Form
+    {
+        public FindPersonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
